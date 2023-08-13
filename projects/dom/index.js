9331f75e4bfe69503024a54183b1bc2d2288ff1b
Daniil Nesteruk <nesterukia@gmail.com>
2023-08-13 20:39:23 +0300

    week-3 hw done

--- projects/dom/index.js
+++ projects/dom/index.js
@@ -11,6 +11,9 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+  const element = document.createElement('div');
+  element.textContent = text;
+  return element;
 }
 
 /*
@@ -22,6 +25,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
+  where.insertBefore(what, where.firstChild);
 }
 
 /*
@@ -44,6 +48,13 @@ function prepend(what, where) {
    findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
+  const result = [];
+  for(const child of where.children){
+    if(child.nextElementSibling && child.nextElementSibling.tagName === 'P'){
+      result.push(child);
+    }
+  }
+    return result;
 }
 
 /*
@@ -66,7 +77,7 @@ function findAllPSiblings(where) {
 function findError(where) {
   const result = [];
 
-  for (const child of where.childNodes) {
+  for (const child of where.children) {
     result.push(child.textContent);
   }
 
@@ -86,6 +97,11 @@ function findError(where) {
    должно быть преобразовано в <div></div><p></p>
  */
 function deleteTextNodes(where) {
+  for(const node of where.childNodes){
+    if (node.nodeType === 3){
+      node.remove();
+    }
+  }
 }
 
 /*
@@ -109,6 +125,39 @@ function deleteTextNodes(where) {
    }
  */
 function collectDOMStat(root) {
+  
+  const result = {
+    tags: {},
+    classes: {},
+    texts: 0
+  };
+
+  function scan(root){
+    for(const node of root.childNodes){
+      if (node.nodeType === 3){
+        result.texts++;
+      } else if(node.nodeType === 1){
+        if (node.tagName in result.tags) {
+          result.tags[node.tagName]++;
+        } else {
+          result.tags[node.tagName] = 1;
+        }
+
+        for(const className of node.classList){
+          if (className in result.classes) {
+            result.classes[className]++;
+          } else {
+            result.classes[className] = 1;
+          }
+        }  
+        scan(node);
+      }
+    }     
+  }
+
+  scan(root);
+
+  return result;
 }
 
 export {
